1e08b66af2a5db80b427cd134c9ef49767fc42ef
Nick Stielau <nick.stielau@gmail.com>
2010-11-12 13:27:25 -0800

    asdf

--- test/truth.test.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'test String#length2': function(assert){
-        assert.equal(6, 'foobar'.length);
-    },
-    'test String#length': function(assert){
-        assert.equal(5, 'fobar'.length);
-    }
-};
